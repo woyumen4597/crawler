44721c7c011b0696031c02b36f3b1c347a5032bb
woyumen4597 <woyumen4597@gmail.com>
2017-09-12 22:54:57 +0800

    Add some files

---
+++ src/main/java/crawer/DailyPixivRanking.java
@@ -0,0 +1,46 @@
+package crawer;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+import com.jayway.jsonpath.DocumentContext;
+import com.jayway.jsonpath.JsonPath;
+
+import us.codecraft.webmagic.Page;
+import us.codecraft.webmagic.Site;
+import us.codecraft.webmagic.Spider;
+import us.codecraft.webmagic.processor.PageProcessor;
+import us.codecraft.webmagic.selector.Json;
+
+public class DailyPixivRanking implements PageProcessor {
+	private static int NUMBER = 5;
+	private Site site = Site.me().setRetryTimes(3).setTimeOut(10000)
+			.setUserAgent("Mozilla/5.0 (Windows; U; Windows NT 5.1; it; rv:1.8.1.11) Gecko/20071127 Firefox/2.0.0.11");
+
+	public void process(Page page) {
+		Json json = page.getJson();
+		DocumentContext context = JsonPath.parse(json.toString());
+		List<String> list = context.read("$.contents[*].url");
+		List<String> urls = new ArrayList<String>();
+		for(int i = 0;i<NUMBER;i++){
+			String url = list.get(i);
+			urls.add(url);
+		}
+		page.putField("urls", urls);
+	}
+
+	public Site getSite() {
+		return site;
+	}
+
+	public static void main(String[] args) {
+		try {
+			Spider.create(new DailyPixivRanking()).addUrl("https://www.pixiv.net/ranking.php?mode=daily&format=json").thread(3)
+					.addPipeline(new PixivPipeLine()).run();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+	}
+}
---
+++ src/main/java/utils/FileUtils.java
@@ -0,0 +1,29 @@
+package utils;
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
+public class FileUtils {
+	public static void readToBuffer(StringBuffer buffer,String filePath) throws IOException{
+		InputStream in = new FileInputStream(filePath);
+		String line;
+		BufferedReader reader = new BufferedReader(new InputStreamReader(in));
+		line = reader.readLine();
+		while(line!=null){
+			buffer.append(line);
+			buffer.append("\n");
+			line = reader.readLine();
+		}
+		reader.close();
+		in.close();
+	}
+	public static String readFile(String filePath) throws IOException{
+		StringBuffer sb = new StringBuffer();
+		readToBuffer(sb, filePath);
+		return sb.toString();
+	}
+
+}
