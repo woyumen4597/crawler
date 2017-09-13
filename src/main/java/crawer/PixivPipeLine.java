e70b35879523394ac79e2d4ac8e879393030a1b7
woyumen4597 <woyumen4597@gmail.com>
2017-09-13 22:35:54 +0800

    Changes to be committed: 	deleted:    src/main/java/crawer/PicturePageProcessor.java 	modified:   src/main/java/crawer/PixivPipeLine.java 	modified:   todo.list

--- src/main/java/crawer/PixivPipeLine.java
+++ src/main/java/crawer/PixivPipeLine.java
@@ -8,20 +8,56 @@ import us.codecraft.webmagic.pipeline.Pipeline;
 import utils.PicUtils;
 
 public class PixivPipeLine implements Pipeline {
-	//下载的目录
-	private String basePath = "D:\\webmagic";
+	// 下载的目录
+	private String basePath = "D:\\webmagic"; // default
+	private int number = 5; // default
+
+	public String getBasePath() {
+		return basePath;
+	}
+
+	public PixivPipeLine setBasePath(String basePath) {
+		this.basePath = basePath;
+		return this;
+	}
+
+	public PixivPipeLine setNumber(int number) {
+		this.number = number;
+		return this;
+	}
+
+	public int getNumber() {
+		return number;
+	}
+
+	public PixivPipeLine() {
+	}
+
+	public PixivPipeLine(String basePath, int number) {
+		this.basePath = basePath;
+		this.number = number;
+	}
+
+	public PixivPipeLine(int number) {
+		this.number = number;
+	}
+
+	public PixivPipeLine(String basePath) {
+		this.basePath = basePath;
+	}
 
 	public void process(ResultItems resultItems, Task task) {
 		List<String> list = resultItems.get("urls");
 
-		for (String url : list) {
+		for (int i = 0; i < this.number; i++) {
+			String url = list.get(i);
 			String filename = url.substring(url.lastIndexOf("/"));
-				try {
-					PicUtils.download(basePath,filename, url);
-				} catch (Exception e) {
-					e.printStackTrace();
-					continue;
-				}
+			try {
+				PicUtils.download(basePath, filename, url);
+			} catch (Exception e) {
+				e.printStackTrace();
+				continue;
+			}
 
 		}
 	}
