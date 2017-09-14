2aa45caafc8e57321ef461d05b913edf49d33003
woyumen4597 <woyumen4597@gmail.com>
2017-09-14 23:29:10 +0800

    modified:   change.log modified:   src/main/java/user/User.java

--- src/main/java/user/User.java
+++ src/main/java/user/User.java
@@ -4,56 +4,22 @@ package user;
 import com.jayway.jsonpath.DocumentContext;
 import com.jayway.jsonpath.JsonPath;
 
+import pipeline.UserPipeLine;
 import us.codecraft.webmagic.Page;
 import us.codecraft.webmagic.Site;
+import us.codecraft.webmagic.Spider;
 import us.codecraft.webmagic.processor.PageProcessor;
 import us.codecraft.webmagic.selector.Json;
 
 public class User implements PageProcessor {
-	private String name;
 	private String id;
-	private String account;
-	private String gender;
-	private String twitter;
+	private Detail detail;
 
-	public String getName() {
-		return name;
+	public User() {
 	}
 
-	public void setName(String name) {
-		this.name = name;
-	}
-
-	public String getId() {
-		return id;
-	}
-
-	public void setId(String id) {
-		this.id = id;
-	}
-
-	public String getAccount() {
-		return account;
-	}
-
-	public void setAccount(String account) {
-		this.account = account;
-	}
-
-	public String getGender() {
-		return gender;
-	}
-
-	public void setGender(String gender) {
-		this.gender = gender;
-	}
-
-	public String getTwitter() {
-		return twitter;
-	}
-
-	public void setTwitter(String twitter) {
-		this.twitter = twitter;
+	public User(String id) {
+		this.setId(id);
 	}
 
 	public void setSite(Site site) {
@@ -66,18 +32,56 @@ public class User implements PageProcessor {
 	public void process(Page page) {
 		Json json = page.getJson();
 		DocumentContext context = JsonPath.parse(json.toString());
-		User user = new User();
-		user.setName(context.read("$.user.name").toString());
-		user.setId(context.read("$.user.id").toString());
-		user.setAccount(context.read("$.user.account").toString());
-		user.setGender(context.read("$.profile.gender").toString());
-		user.setTwitter(context.read("$.profile.twitter_url").toString());
-		page.putField("user", user);
+		Detail detail = new Detail();
+		detail.setName(context.read("$.user.name").toString());
+		detail.setId(context.read("$.user.id").toString());
+		detail.setAccount(context.read("$.user.account").toString());
+		detail.setGender(context.read("$.profile.gender").toString());
+		detail.setTwitter(context.read("$.profile.twitter_url").toString());
+		page.putField("detail", detail);
+		setDetail(detail);
 
 	}
+
 
 	public Site getSite() {
 		return site;
 	}
+
+	public Detail detail(){
+		if(getId()==null||getId().equals("")){
+			throw new RuntimeException("Please input user_id");
+		}
+		String url = "https://app-api.pixiv.net/v1/user/detail?user_id="+getId();
+		Detail detail = new Detail();
+		UserPipeLine pipeLine = new UserPipeLine(detail);
+		Spider.create(new User()).addUrl(url).thread(3).addPipeline(pipeLine).run();
+		return pipeLine.getDetail();
+	}
+
+	public Detail detail(String id){
+		String url = "https://app-api.pixiv.net/v1/user/detail?user_id="+id;
+		Detail detail = new Detail();
+		UserPipeLine pipeLine = new UserPipeLine(detail);
+		Spider.create(new User()).addUrl(url).thread(3).addPipeline(pipeLine).run();
+		return pipeLine.getDetail();
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public Detail getDetail() {
+		return detail;
+	}
+
+	public void setDetail(Detail detail) {
+		this.detail = detail;
+	}
+
 
 }
