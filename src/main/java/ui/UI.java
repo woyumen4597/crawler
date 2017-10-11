c94c05ea88822d3eabd7a1db5b1f4bf9dd387b0c
jrc <woyumen4597@gmail.com>
2017-10-11 21:26:23 +0800

    Add Download Module

--- src/main/java/ui/UI.java
+++ src/main/java/ui/UI.java
@@ -1,21 +1,27 @@
 package ui;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.imageio.ImageIO;
 
 import auth.Auth;
 import crawer.Pixivision;
 import crawer.Rank;
 import javafx.application.Application;
 import javafx.application.Platform;
+import javafx.embed.swing.SwingFXUtils;
 import javafx.event.ActionEvent;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.ContextMenu;
 import javafx.scene.control.Hyperlink;
 import javafx.scene.control.Label;
+import javafx.scene.control.MenuItem;
 import javafx.scene.control.Pagination;
 import javafx.scene.control.TextField;
 import javafx.scene.control.TreeItem;
@@ -23,6 +29,8 @@ import javafx.scene.control.TreeView;
 import javafx.scene.effect.DropShadow;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseButton;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.Background;
 import javafx.scene.layout.BackgroundFill;
@@ -35,6 +43,7 @@ import javafx.scene.layout.CornerRadii;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
+import javafx.stage.FileChooser;
 import javafx.stage.Stage;
 import search.Search;
 import user.User;
@@ -42,11 +51,13 @@ import utils.DuplicateRemover;
 
 public class UI extends Application {
 	private List<String> urls = new ArrayList<String>();
+	private Stage primaryStage;
 	private Pagination pagination;
 	private AnchorPane anchor;
 	private Image favicon = new Image(getClass().getResourceAsStream("pixiv.jpg"));
-	private BackgroundImage bImage = new BackgroundImage(new Image(getClass().getResourceAsStream("background.jpg")), BackgroundRepeat.REPEAT, BackgroundRepeat.REPEAT,
-			BackgroundPosition.DEFAULT, BackgroundSize.DEFAULT);
+	private BackgroundImage bImage = new BackgroundImage(new Image(getClass().getResourceAsStream("background.jpg")),
+			BackgroundRepeat.REPEAT, BackgroundRepeat.REPEAT, BackgroundPosition.DEFAULT, BackgroundSize.DEFAULT);
+
 	public int itemsPerpage() {
 		return 4;
 	}
@@ -54,6 +65,7 @@ public class UI extends Application {
 	@SuppressWarnings({ "rawtypes", "unchecked" })
 	@Override
 	public void start(Stage primaryStage) throws Exception {
+		this.primaryStage = primaryStage;
 		primaryStage.setTitle("Pixiv4J");
 		BorderPane pane = new BorderPane();
 		anchor = new AnchorPane();
@@ -82,7 +94,7 @@ public class UI extends Application {
 		VBox vBox = new VBox();
 		vBox.setEffect(new DropShadow());
 		vBox.setSpacing(5);
-		vBox.setBackground(new Background(new BackgroundFill(Color.LIGHTBLUE,new CornerRadii(5),new Insets(0))));
+		vBox.setBackground(new Background(new BackgroundFill(Color.LIGHTBLUE, new CornerRadii(5), new Insets(0))));
 		vBox.setPadding(new Insets(6));
 		TreeItem rankItem = new TreeItem("Rank");
 		rankItem.setExpanded(true);
@@ -154,14 +166,13 @@ public class UI extends Application {
 		TreeView userTree = new TreeView(userItem);
 		vBox.getChildren().addAll(hBox, rankTree, userTree);
 		// stage
-		
+
 		pane.setBackground(new Background(bImage));
 		pane.setLeft(vBox);
 		pane.setCenter(anchor);
 		Scene scene = new Scene(pane);
 		primaryStage.setScene(scene);
-		
-		
+
 		primaryStage.getIcons().add(favicon);
 		primaryStage.setResizable(true);
 		primaryStage.setMaximized(true);
@@ -221,16 +232,46 @@ public class UI extends Application {
 		VBox box = new VBox(5);
 		int page = pageIndex * itemsPerpage();
 		for (int i = page; i < page + itemsPerpage(); i++) {
+			String name = urls.get(i).substring(urls.get(i).lastIndexOf("/")+1);
 			Image image = new Image(urls.get(i));
+			ContextMenu cm = new ContextMenu();
 			if (!image.isError()) {
 				ImageView imageView = new ImageView(image);
+				imageView.addEventHandler(MouseEvent.MOUSE_CLICKED, (MouseEvent e) -> {
+					if (e.getButton() == MouseButton.SECONDARY) {
+						cm.show(imageView, e.getScreenX(), e.getScreenY());
+					}
+				});
+				MenuItem cmItem = new MenuItem("Save Image");
+				cm.getItems().add(cmItem);
+				cmItem.setOnAction((ActionEvent e) -> {
+					FileChooser fileChooser1 = new FileChooser();
+					fileChooser1.setTitle("Save Image");
+					FileChooser.ExtensionFilter filter = new FileChooser.ExtensionFilter("JPG FILES","*.jpg");
+					FileChooser.ExtensionFilter filter2 = new FileChooser.ExtensionFilter("PNG FILES","*.png");
+					fileChooser1.getExtensionFilters().addAll(filter,filter2);
+					fileChooser1.setInitialFileName(name);
+					fileChooser1.setInitialDirectory(new File(System.getProperty("user.dir")));
+					File file = fileChooser1.showSaveDialog(primaryStage);
+					if (file != null) {
+						try {
+							ImageIO.write(SwingFXUtils.fromFXImage(image, null), "jpg", file);
+						} catch (IOException ex) {
+							System.out.println(ex.getMessage());
+						}
+					}
+
+				});
+
 				imageView.setFitHeight(150);
 				imageView.setFitWidth(150);
+
 				box.getChildren().add(imageView);
 				box.setAlignment(Pos.CENTER);
 			}
 
 		}
+
 		return box;
 	}
 
